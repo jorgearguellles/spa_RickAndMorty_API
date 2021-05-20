8d38d8f7405f5b80b4d41813a425eb05c68ea1f3
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-05-20 15:22:59 -0500

    add/ Error404.js & Update/Characters.js & Home.js

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,19 +1,19 @@
 const Character = () => {
 	const view = `
-  <div class="Characters-inner">
-    <article class="Characters-card">
-      <img src="image" alt="name character" />
-      <h2>Name Character</h2>
-    </article>
-    <article class="Characters-card">
-      <h3>Episodes:</h3>
-      <h3>Status:</h3>
-      <h3>Specie:</h3>
-      <h3>Gender:</h3>
-      <h3>Origin:</h3>
-      <h3>Last location:</h3>
-    </article>
-  </div>
+    <div class="Characters-inner">
+      <article class="Characters-card">
+        <img src="image" alt="name character" />
+        <h2>Name Character</h2>
+      </article>
+      <article class="Characters-card">
+        <h3>Episodes:</h3>
+        <h3>Status:</h3>
+        <h3>Specie:</h3>
+        <h3>Gender:</h3>
+        <h3>Origin:</h3>
+        <h3>Last location:</h3>
+      </article>
+    </div>
   `;
 	return view;
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 const Home = () => {
 	const view = `
-  <div class="Characters">
-    <article class="Character-item">
-      <a href="#/1/">
-        <img src="image" alt="name character" />
-        <h2>Name character</h1>
-      </a>
-    </article>
-  </div>
+    <div class="Characters">
+      <article class="Character-item">
+        <a href="#/1/">
+          <img src="image" alt="name character" />
+          <h2>Name character</h1>
+        </a>
+      </article>
+    </div>
   `;
 	return view;
 };
