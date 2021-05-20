64614abdec5686597aa1059e768f49d03de479a6
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-05-20 16:20:45 -0500

    add/src/utils/getHash.js & resolveRoutes.js && update/src/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from "../templates/Header.js";
 import Home from "../pages/Home.js";
 import Character from "../pages/Character.js";
 import Error404 from "../pages/Error404.js";
+import getHash from "../utils/getHash.js";
+import resolveRoutes from "../utils/resolveRoutes.js";
 
 const routes = {
 	"/": Home,
